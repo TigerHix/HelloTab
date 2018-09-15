775f61beae8de19925ff53e9134b1a72e7a0f088
James <zhaoziqi200@gmail.com>
2018-09-15 18:49:58 -0400

    Added To-Do List & more
    Added a To-Do List on a card and added a new card template without the image header. Changed Home.vue to add the To-Do List card.

---
+++ src/components/ToDoListCard.vue
@@ -0,0 +1,8 @@
+<template>
+  <b-card title="To-Do List"
+        tag="article"
+        style="max-width: 20rem;"
+        class="mb-2">
+    <slot/>
+  </b-card>
+</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="home">
     <Reddit/>
+    <ToDoList/>
   </div>
 </template>
 
 <script>
 import Reddit from '@/components/Reddit'
+import ToDoList from '@/components/ToDoList'
 export default {
   name: 'home',
   components: {
-    Reddit
+    Reddit,
+    ToDoList
   }
 }
 </script>
