ab98f77bc086924624e6adf0eccdaee45e992a58
James <zhaoziqi200@gmail.com>
2018-09-15 21:42:09 -0400

    Twitter Card
    Adds a Twitter Timeline feed onto a card. The content will be spread out based on the size of the card, so it can be put onto a larger card.

---
+++ src/components/TwitterFeedCard.vue
@@ -0,0 +1,8 @@
+<template>
+  <b-card title="Twitter Feed"
+        tag="article"
+        style="max-width: 40rem;"
+        class="mb-2">
+    <slot/>
+  </b-card>
+</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,20 @@
   <div class="home">
     <Reddit/>
     <ToDoList/>
+    <TwitterFeed/>
   </div>
 </template>
 
 <script>
 import Reddit from '@/components/Reddit'
 import ToDoList from '@/components/ToDoList'
+import TwitterFeed from '@/components/TwitterFeed'
 export default {
   name: 'home',
   components: {
     Reddit,
-    ToDoList
+    ToDoList,
+    TwitterFeed
   }
 }
 </script>
