c8877fff2c2bc5ac9ca12ee00a431b591d260441
James <zhaoziqi200@gmail.com>
2018-09-16 10:45:31 -0400

    Fixes to Math Questions and Twitter Feed
    Made things appear on the right layer by changing z-index. Math Challenge is now functional albeit really hack-y

--- src/components/MathQuestion.vue
+++ src/components/MathQuestion.vue
@@ -1,15 +1,18 @@
 <template>
   <card>
-    <h4>Score: {{score}}</h4>
-    <h4>Timer: {{timer}}</h4>
-    <p id="mathQuestion" v-html="question" class="h3 mb-3"></p>
-    <div class="mb-2" v-for="(choice, index) in choices">
-      <button class="btn btn-primary btn-block" v-html="choice" @click="checkAnswer(index)"></button>
+    <div slot="title">Math Question</div>
+    <div id="math-quesion" class="mb-3 mx-3" style="z-index:100; position: relative;">
+      <h4 style="z-index:100;">Score: {{score}}</h4>
+      <h4 style="z-index:100;">Timer: {{timer}}</h4>
+      <p id="mathQuestion" v-html="question" class="h3 mb-3"></p>
+      <div class="mb-2" style="z-index:100;" v-for="(choice, index) in choices">
+        <button class="btn btn-sm btn-primary btn-block" style="z-index:100;" v-html="choice" @click="checkAnswer(index)"></button>
+      </div>
+      <b-button-group style="width:100%;">
+        <b-button variant="outline-light col" @click="btn1Function" :disabled="!canRequestQuestion">{{btn1Text}}</b-button>
+        <button class="btn btn-outline-primary btn-block col" @click="btn2Function">{{btn2Text}}</button>
+      </b-button-group>
     </div>
-    <b-button-group style="width:100%;">
-      <b-button variant="outline-dark col" @click="" :disabled="!canRequestQuestion">{{btn1Text}}</b-button>
-      <button class="btn btn-outline-primary btn-block col" @click="startChallenge">{{btn2Text}}</button>
-    </b-button-group>
   </card>
 </template>
 <script>
@@ -28,23 +31,55 @@ export default {
 
       correct: 0,
       question: "",
-      choices: [],
+      choices: ["-", "-", "-", "-", "-"],
 
       tempQ: "",
       tempC: [],
+      tempT: 0,
 
       timer: 0,
       score: 0,
 
       btn1Text: "-",
       btn2Text: "Start Challenge",
+      btn1State: 0,                 // 0 = "-", 1 = "Quit"
+      btn2State: 0,                 // 0 = "Start", 1 = "Pause", 2 = "Resume"
     }
   },
   methods: {
+    btn1Function() {
+      switch(this.btn1State) {
+        case 0:
+          break;
+        case 1:
+          this.quitChallenge();
+          this.question = "";
+          this.choices = ["-", "-", "-", "-", "-"];
+          break;
+      }
+    },
+
+    btn2Function() {
+      switch(this.btn2State) {
+        case 0:
+          this.startChallenge();
+          break;
+        case 1:
+          this.pauseChallenge();
+          break;
+        case 2:
+          this.resumeChallenge();
+          break;
+      }
+    },
+
     startChallenge() {
       this.timer = 30;
       this.getQuestion();
       this.mainLoop();
+      this.score = 0;
+      this.btn1State = 1;
+      this.btn2State = 1;
       this.btn1Text = "Quit";
       this.btn2Text = "Pause";
       // set btn 2 function to pauseChallenge()
@@ -53,8 +88,11 @@ export default {
     pauseChallenge() {
       this.tempQ = this.question;
       this.tempC = this.choices;
+      this.tempT = this.timer;
       this.question = "";
-      this.choices = [];
+      this.choices = ["-", "-", "-", "-", "-"];
+      this.timer = -1;
+      this.btn2State = 2;
       this.btn2Text = "resume";
       // change btn2 function to resumeChallenge
     },
@@ -62,11 +100,18 @@ export default {
     resumeChallenge() {
       this.question = this.tempQ;
       this.choices = this.tempC;
+      this.timer = this.tempT;
+      this.btn2State = 1;
       this.btn2Text = "Pause";
+      this.mainLoop();
       // change btn2 function to pauseChallenge
     },
 
     quitChallenge() {
+      this.btn1State = 0;
+      this.btn2State = 0;
+      this.btn1Text = "-";
+      this.btn2Text = "Start Challenge";
       this.timer = 0;
     },
 
@@ -75,12 +120,21 @@ export default {
         this.timer -= 1;
         var x = setTimeout(() => { this.mainLoop(); }, 1000);
       }
+      else if (this.timer == 0) {
+        this.btn1Text = "-";
+        this.btn2Text = "Start Challenge";
+        this.btn1State = 0;
+        this.btn2State = 0;
+        this.question = "";
+        this.choices = ["-", "-", "-", "-", "-"];
+        window.alert("Challenge over! Score: " + this.score);
+      }
     },
 
     getQuestion() {
       if(this.canRequestQuestion == true) {
         this.choices = [];
-        axios.get('https://math.ly/api/v1/algebra/linear-equations.json')
+        axios.get('https://math.ly/api/v1/algebra/linear-equations.json?difficulty=beginner')
         .then(response => {
           this.question = response.data.question;
           this.correct = response.data.correct_choice;
@@ -90,18 +144,19 @@ export default {
           }
         })
         this.canRequestQuestion = false;
-        setTimeout (()=>{ this.canRequestQuestion = true; }, 3000);
+        setTimeout (()=>{ this.canRequestQuestion = true; }, 1500);
       }
     },
 
     checkAnswer(index) {
       var correctAnswer = this.correct;
-      console.log(correctAnswer);
-      // console.log(index);
-      // for(var i = 0; i < this.choices.length(); i++) {
-      //
-      // }
-      // console.log(choice);
+      console.log("Index: " + index);
+      console.log("Correct Answer: " + correctAnswer);
+      if (index == correctAnswer) {
+        this.score += 1;
+        console.log("correct!");
+      }
+      this.getQuestion();
     }
   },
 
--- src/components/TwitterFeed.vue
+++ src/components/TwitterFeed.vue
@@ -1,7 +1,9 @@
 <template>
   <card>
-    <a class="twitter-grid" href="https://twitter.com/TwitterDev/timelines/539487832448843776?ref_src=twsrc%5Etfw">National Park Tweets</a>
-    <!-- <iframe src="twitter.com"></iframe> -->
+    <div slot="title">Twitter Feed</div>
+    <div class="mb-2 mx-2" style="position: relative;">
+      <a class="twitter-grid" href="https://twitter.com/TwitterDev/timelines/539487832448843776?ref_src=twsrc%5Etfw">National Park Tweets</a>
+    </div>
   </card>
 </template>
 <script async src="" charset="utf-8"></script>
@@ -23,11 +25,9 @@ export default {
   },
 
   mounted() {
-    /*
     var twitterFeedScript = document.createElement('script');
     twitterFeedScript.setAttribute('src', 'https://platform.twitter.com/widgets.js');
     document.head.appendChild(twitterFeedScript);
-    */
   },
 }
 </script>
